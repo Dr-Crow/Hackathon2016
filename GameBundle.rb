7bb7e3127f4b6fea9827872e51dd868080bfe8ec
John <John.Randis1@Marist.edu>
2015-04-18 02:39:00 -0400

    AI TicTacToe complete

--- GameBundle.rb
+++ GameBundle.rb
@@ -77,23 +77,116 @@ end
 
 def AImove()
 
+	num = 1 + rand(9)
+	puts num
+	placed = false
+	while placed == false
+		case num
+			when 1
+				if $x1[0] == " "
+					$x1[0] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+			when 2
+				if $x1[1] == " "
+					$x1[1] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+			when 3
+				if $x1[2] == " "
+					$x1[2] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+			when 4
+				if $x2[0] == " "
+					$x2[0] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+			when 5
+				if $x2[1] == " "
+					$x2[1] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+			when 6
+				if $x2[2] == " "
+					$x2[2] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+			when 7
+				if $x3[0] == " "
+					$x3[0] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+			when 8
+				if $x3[1] == " "
+					$x3[1] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+			when 9
+				if $x3[2] == " "
+					$x3[2] = "O" 
+					placed = true
+				else
+					AImove()
+				end
+		end
+		placed = true
+	end
+	
+		if $x1[0] == "O" and $x1[1] == "O" and $x1[2] == "O"
+			$loss = true
+			$canRun = false
+		elsif $x2[0] == "O" and $x2[1] == "O" and $x2[2] == "O" 
+			$loss = true
+			$canRun = false
+		elsif $x3[0] == "O" and $x3[1] == "O" and $x3[2] == "O" 
+			$loss = true
+			$canRun = false
+		elsif $x1[0] == "O" and $x2[0] == "O" and $x3[0] == "O"
+			$loss = true
+			$canRun = false
+		elsif $x1[1] == "O" and $x2[1] == "O" and $x3[1] == "O"
+			$loss = true
+			$canRun = false
+		elsif $x1[2] == "O" and $x2[2] == "O" and $x3[2] == "O"
+			$loss = true
+			$canRun = false
+		end
+		
+end
 
+	$x1 = [" ", " ", " "]
+	$x2 = [" ", " ", " "]
+	$x3 = [" ", " ", " "]
+	line1 = $x1[0]+"|"+$x1[1]+"|"+$x1[2]
+	line2 = $x2[0]+"|"+$x2[1]+"|"+$x2[2]
+	line3 = $x3[0]+"|"+$x3[1]+"|"+$x3[2]
 
 def gameLoop() 
 
-	x1 = [" ", " ", " "]
-	x2 = [" ", " ", " "]
-	x3 = [" ", " ", " "]
-	line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
-	line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
-	line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
-
-	canRun = true
+	$loss = false
+	$canRun = true
 	
-	while canRun == true
-		line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
-		line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
-		line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
+	while $canRun == true
+		line1 = $x1[0]+"|"+$x1[1]+"|"+$x1[2]
+		line2 = $x2[0]+"|"+$x2[1]+"|"+$x2[2]
+		line3 = $x3[0]+"|"+$x3[1]+"|"+$x3[2]
 		puts "\n" + line1
 		puts "------"
 		puts line2
@@ -103,51 +196,94 @@ def gameLoop()
 		move = gets.upcase.chomp
 		case move
 			when "TL"
-			x1[0] = "X"
+				if $x1[0] == " "
+					$x1[0] = "X"
+				else
+					puts "That spot is taken!"
+				end
 			when "TM"
-			x1[1] = "X"
+				if $x1[1] == " "
+					$x1[1] = "X"
+				else
+					puts "That spot is taken!"
+				end
 			when "TR"
-			x1[2] = "X"
+				if $x1[2] == " "
+					$x1[2] = "X"
+				else
+					puts "That spot is taken!"
+				end
 			when "ML"
-			x2[0] = "X"
+				if $x2[0] == " "
+					$x2[0] = "X"
+				else
+					puts "That spot is taken!"
+				end
 			when "M"
-			x2[1] = "X"
+				if $x2[1] == " "
+					$x2[1] = "X"
+				else
+					puts "That spot is taken!"
+				end
 			when "MR"
-			x2[2] = "X"
+				if $x2[2] == " "
+					$x2[2] = "X"
+				else
+					puts "That spot is taken!"
+				end
 			when "BL"
-			x3[0] = "X"
+				if $x3[0] == " "
+					$x3[0] = "X"
+				else
+					puts "That spot is taken!"
+				end
 			when "BM"
-			x3[1] = "X"
+				if $x3[1] == " "
+					$x3[1] = "X"
+				else
+					puts "That spot is taken!"
+				end
 			when "BR"
-			x3[2] = "X"
+				if $x3[2] == " "
+					$x3[2] = "X"
+				else
+					puts "That spot is taken!"
+				end
 		end		
 		
-		if x1[0] == "X" and x1[1] == "X" and x1[2] == "X"
-			canRun = false
-		elsif x2[0] == "X" and x2[1] == "X" and x2[2] == "X" 
-			canRun = false
-		elsif x3[0] == "X" and x3[1] == "X" and x3[2] == "X" 
-			canRun = false
-		elsif x1[0] == "X" and x2[0] == "X" and x3[0] == "X"
-			canRun = false
-		elsif x1[1] == "X" and x2[1] == "X" and x3[1] == "X"
-			canRun = false
-		elsif x1[2] == "X" and x2[2] == "X" and x3[2] == "X"
-			canRun = false
+		puts "The AI is moving..."
+		AImove()
+		
+		if $x1[0] == "X" and $x1[1] == "X" and $x1[2] == "X"
+			$canRun = false
+		elsif $x2[0] == "X" and $x2[1] == "X" and $x2[2] == "X" 
+			$canRun = false
+		elsif $x3[0] == "X" and $x3[1] == "X" and $x3[2] == "X" 
+			$canRun = false
+		elsif $x1[0] == "X" and $x2[0] == "X" and $x3[0] == "X"
+			$canRun = false
+		elsif $x1[1] == "X" and $x2[1] == "X" and $x3[1] == "X"
+			$canRun = false
+		elsif $x1[2] == "X" and $x2[2] == "X" and $x3[2] == "X"
+			$canRun = false
 		end
 		
 	end
 	
-		line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
-		line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
-		line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
+		line1 = $x1[0]+"|"+$x1[1]+"|"+$x1[2]
+		line2 = $x2[0]+"|"+$x2[1]+"|"+$x2[2]
+		line3 = $x3[0]+"|"+$x3[1]+"|"+$x3[2]
 		puts "\n" + line1
 		puts "------"
 		puts line2
 		puts "------"
 		puts line3 + "\n"
-	
-	puts "Congrats, you win!"
+		
+	if $loss == true
+		print "sorry, you lose!!!!!!!!!!!!!!"
+	else
+		puts "Congrats, you win!"
+	end
 
 end
 
