8fb4669bc3bdd88ce16f46c73618033e82b5a555
John <John.Randis1@Marist.edu>
2015-04-17 23:37:24 -0400

    Can add Xs to proper places in board

--- GameBundle.rb
+++ GameBundle.rb
@@ -59,8 +59,78 @@ def hangManPlayer2()
 
 end
 
-def ticTacToe()
 
+
+
+def ticTacToe()
+     puts "Your are X. Your opponent is O. The commands are as follows: \n"
+	 puts "\nTL to place an X in the top left spot"
+	 puts "TM to place an X in the top middle spot"
+	 puts "TR to place an X in the top right spot"
+	 puts "ML to place an X in the middle left spot"
+	 puts "M to place an X in the middle spot"
+	 puts "MR to place an X in the middle right spot"
+	 puts "BL to place an X in the bottom left spot"
+	 puts "BM to place an X in the bottom middle spot"
+	 puts "BR to place an X in the bottom right spot\n"
+	 gameLoop
+
+end
+
+def gameLoop()
+
+x1 = [" ", " ", " "]
+x2 = [" ", " ", " "]
+x3 = [" ", " ", " "]
+$line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
+$line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
+$line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
+
+	canRun = true
+	
+	while canRun == true
+		$line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
+		$line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
+		$line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
+		puts "\n" + $line1
+		puts "------"
+		puts $line2
+		puts "------"
+		puts $line3 + "\n"
+		puts "\n Make a move."
+		move = gets.chomp
+		case move
+			when "TL"
+			#$line1 = "X| | "
+			x1[0] = "X"
+			when "TM"
+			#$line1 = " |X| "
+			x1[1] = "X"
+			when "TR"
+			#$line1 = " | |X"
+			x1[2] = "X"
+			when "ML"
+			#$line2 = "X| | "
+			x2[0] = "X"
+			when "M"
+			#$line2 = " |X| "
+			x2[1] = "X"
+			when "MR"
+			#$line2 = " | |X"
+			x2[2] = "X"
+			when "BR"
+			#$line3 = "X| | "
+			x3[0] = "X"
+			when "BM"
+			#$line3 = " |X| "
+			x3[1] = "X"
+			when "BL"
+			#$line3 = " | |X"
+			x3[2] = "X"
+		end
+		
+		
+	end
 
 end
 
