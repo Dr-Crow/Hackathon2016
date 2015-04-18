82ec7d5096af9e2dfe6df9302988112449c9f2a2
unknown <James.Crowley1@marist.edu>
2015-04-18 17:59:50 -0400

    Working on it

--- Card.rb
+++ Card.rb
@@ -1,68 +1,59 @@
-class Cards
-	$deck = []
+class Card
+	attr_accessor(:value, :suit)
+
 	def initialize(value, suit)
-	  @value = value
-	  @suit = suit
+		@value = value
+		@suit = suit
 	end
-	def toString
-	  if(Integer(@value) == 1)
-		  puts " _____ "
-		  puts "|" + "A" + "    |"
-		  puts "|     |"
-		  puts "|  " + @suit + "  |"
-		  puts "|     |"
-		  puts "|____" + "A" +  "|"
-	  elsif(Integer(@value) < 10)
-		  puts " _____ "
-		  puts "|" + @value + "    |"
-		  puts "|     |"
-		  puts "|  " + @suit + "  |"
-		  puts "|     |"
-		  puts "|____" + @value +  "|"
-	  elsif(Integer(@value) == 10)
-		  puts " _____ "
-		  puts "|" + @value + "   |"
-		  puts "|     |"
-		  puts "|  " + @suit + "  |"
-		  puts "|     |"
-		  puts "|___" + @value +  "|"
-	  elsif(Integer(@value) == 11)
-		  puts " _____ "
-		  puts "|" + "J" + "    |"
-		  puts "|     |"
-		  puts "|  " + @suit + "  |"
-		  puts "|     |"
-		  puts "|____" + "J" +  "|"
-	  elsif(Integer(@value) == 12)
-		  puts " _____ "
-		  puts "|" + "Q" + "    |"
-		  puts "|     |"
-		  puts "|  " + @suit + "  |"
-		  puts "|     |"
-		  puts "|____" + "Q" +  "|"
-	  elsif(Integer(@value) == 13)
-		  puts " _____ "
-		  puts "|" + "K" + "    |"
-		  puts "|     |"
-		  puts "|  " + @suit + "  |"
-		  puts "|     |"
-		  puts "|____" + "K" +  "|"
-	  end
-	  
-	end
-	
-	encoded = "\u2665".encode('utf-8')
-	
-	for i in 1..13
-	  n = String(i)
-	  $deck.push(Cards.new(n, "\u2665".encode('utf-8')  ) )
-	  puts $deck[i-1].toString
-	end
-	
 
-	
+	def self.toString
+		if(@value == 1)
+			puts " _____ "
+			puts "|" + "A" + "    |"
+			puts "|     |"
+			puts "|  " + @suit + "  |"
+			puts "|     |"
+			puts "|____" + "A" +  "|"
+		elsif(@value < 10)
+			puts " _____ "
+			puts "|" + @value + "    |"
+			puts "|     |"
+			puts "|  " + @suit + "  |"
+			puts "|     |"
+			puts "|____" + @value +  "|"
+		elsif (@value == 10)
+			puts " _____ "
+			puts "|" + @value + "   |"
+			puts "|     |"
+			puts "|  " + @suit + "  |"
+			puts "|     |"
+			puts "|___" + @value +  "|"
+		elsif(@value == 11)
+			puts " _____ "
+			puts "|" + "J" + "    |"
+			puts "|     |"
+			puts "|  " + @suit + "  |"
+			puts "|     |"
+			puts "|____" + "J" +  "|"
+		elsif(@value == 12)
+			puts " _____ "
+			puts "|" + "Q" + "    |"
+			puts "|     |"
+			puts "|  " + @suit + "  |"
+			puts "|     |"
+			puts "|____" + "Q" +  "|"
+		elsif(@value == 13)
+			puts " _____ "
+			puts "|" + "K" + "    |"
+			puts "|     |"
+			puts "|  " + @suit + "  |"
+			puts "|     |"
+			puts "|____" + "K" +  "|"
+		end
 
+	end
 end
+
 
 =begin 
 	 \u2660 - black spade
