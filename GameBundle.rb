db2f8cc1527ad6a87447f3bf5d8191f1207128fd
unknown <James.Crowley1@marist.edu>
2015-04-18 03:28:20 -0400

    Added two player and finished 1 player

--- GameBundle.rb
+++ GameBundle.rb
@@ -6,22 +6,17 @@ $listOfWords.push("hackathon")
 
 $lettersGuessed = Array.new
 
-$wordNumber = rand($listOfWords.size)
-$word = $listOfWords.at($wordNumber)
-$wordLetters = $word.split(//)
-$blank = Array.new($word.size)
-
 $misses = 0
-#ssass
-$x1 = " "
-$x2 = " "
-$x3 = " "
-$x4 = " "
-$x5 = " "
-$x6 = " "
-$x7 = " "
-$x8 = " "
-$x9 = " "
+
+$y1 = " "
+$y2 = " "
+$y3 = " "
+$y4 = " "
+$y5 = " "
+$y6 = " "
+$y7 = " "
+$y8 = " "
+$y9 = " "
 
 
 
@@ -32,7 +27,12 @@ def hangMan()
   puts "Please Enter 1 or 2 for how many players there are: "
   player = Integer(gets.chop)
   if player == 1
+    $wordNumber = rand($listOfWords.size)
+    $word = $listOfWords.at($wordNumber)
+    $wordLetters = $word.split(//)
+    $blank = Array.new($word.size)
     hangManPlayer1
+
   elsif player == 2
     hangManPlayer2
   end
@@ -45,10 +45,8 @@ def hangManPlayer1()
   printOutPic
   puts
   puts printWord
-  puts $misses
   while ($blank.include? '_') && ($misses <= 9)
     hangManStep
-    $misses
   end
 
   if $misses >= 10
@@ -79,11 +77,11 @@ end
 
 def printOutPic()
   puts "     ___    "
-  puts "    |   " + $x1
-  puts "    |  " + $x3 + " " + $x2
-  puts "    |  " + $x6 + $x4 + $x5
-  puts "    |   " + $x7
-  puts "  	|  "+ $x8 + " " + $x9
+  puts "    |   " + $y1
+  puts "    |  " + $y3 + " " + $y2
+  puts "    |  " + $y6 + $y4 + $y5
+  puts "    |   " + $y7
+  puts "  	|  "+ $y8 + " " + $y9
   puts "____|______"
 
 end
@@ -98,42 +96,41 @@ end
 
 def checkGuessVWord()
   count = false
- for x in 0 ... $lettersGuessed.size
-   for y in 0 ... $word.size
-     #puts "(" + $lettersGuessed.at(x) + ", " + $wordLetters.at(y) + ")"
-     if $lettersGuessed.at(x) == $wordLetters.at(y)
-        $blank[y] = $lettersGuessed[x]
-       count = true
+    for y in 0 ... $word.size
+      #puts "(" + $lettersGuessed.at(x) + ", " + $wordLetters.at(y) + ")"
+      if $lettersGuessed.at($lettersGuessed.size - 1) == $wordLetters.at(y)
+        $blank[y] = $lettersGuessed[$lettersGuessed.size - 1]
+        count = true
 
-     end
-   end
- end
+      end
+    end
   if count == false
     $misses+=1
     updatePic
+
   end
 end
 
 def updatePic()
   case $misses
     when 1
-      $x1 = '|'
+      $y1 = '|'
     when 2
-      $x2 = ')'
+      $y2 = ')'
     when 3
-      $x3 = '('
+      $y3 = '('
     when 4
-      $x4 = '|'
+      $y4 = '|'
     when 5
-      $x5 = '/'
+      $y5 = '/'
     when 6
-      $x6 = '\\'
+      $y6 = '\\'
     when 7
-      $x7 = '|'
+      $y7 = '|'
     when 8
-      $x8 = '/'
+      $y8 = '/'
     when 9
-      $x9 = '\\'
+      $y9 = '\\'
   end
 end
 
@@ -148,8 +145,14 @@ def blanks()
 end
 
 def hangManPlayer2()
+  puts "Player 2 Please Enter your word, make sure Player1 does not see: "
+  $word = gets.chop
+  $wordLetters = $word.split(//)
+  $blank = Array.new($word.size)
 
+  hangManPlayer1
 end
+
 def ticTacToe()
 
 end
