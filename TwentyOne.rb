67817b8f9d472328e8d4efcf3bbdd28709a12132
John <John.Randis1@Marist.edu>
2015-04-18 19:24:54 -0400

    please work oh god

---
+++ TwentyOne.rb
@@ -0,0 +1,41 @@
+
+
+class TwentyOne
+require_relative "Card"
+	def initialize
+	end
+	def twentyOne
+		puts "Welcome to Twenty-One! Type \"hit\" and you be dealt a card. Type \"stop\" to stop." 
+		$cards = 52
+		$total = 0
+		
+		$deckObj = Deck.new
+		$deck = $deckObj.createCards()
+		$outOfDeck = Array.new
+		gameLoop()
+	end
+	def gameLoop()
+	  while $total <= 21
+		  if gets.chomp == "hit"
+			hit()
+		  elsif gets.chomp == "stop"
+			puts "Your total was " + $total.to_s
+			if $total < 
+		  end
+	  end
+	
+	end
+	
+	def hit()
+		
+		num = 1 + rand($cards)
+		currentCard = $deck[num]
+		currentCard.toString
+		$total = $total + currentCard.value.to_i
+		puts "Your total is " + $total.to_s
+		
+	end
+	
+
+
+end
