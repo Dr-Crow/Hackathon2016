89e980d49c59f2c18ba8abbd0248377679d0a31e
John <John.Randis1@Marist.edu>
2015-04-18 00:43:26 -0400

    cleaned up code

--- GameBundle.rb
+++ GameBundle.rb
@@ -60,8 +60,6 @@ def hangManPlayer2()
 end
 
 
-
-
 def ticTacToe()
      puts "Your are X. Your opponent is O. The commands are as follows: \n"
 	 puts "\nTL to place an X in the top left spot"
@@ -77,60 +75,79 @@ def ticTacToe()
 
 end
 
-def gameLoop()
+def AImove()
 
-x1 = [" ", " ", " "]
-x2 = [" ", " ", " "]
-x3 = [" ", " ", " "]
-$line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
-$line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
-$line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
+
+
+def gameLoop() 
+
+	x1 = [" ", " ", " "]
+	x2 = [" ", " ", " "]
+	x3 = [" ", " ", " "]
+	line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
+	line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
+	line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
 
 	canRun = true
 	
 	while canRun == true
-		$line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
-		$line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
-		$line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
-		puts "\n" + $line1
+		line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
+		line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
+		line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
+		puts "\n" + line1
 		puts "------"
-		puts $line2
+		puts line2
 		puts "------"
-		puts $line3 + "\n"
-		puts "\n Make a move."
-		move = gets.chomp
+		puts line3 + "\n"
+		puts "\nIt's your turn."
+		move = gets.upcase.chomp
 		case move
 			when "TL"
-			#$line1 = "X| | "
 			x1[0] = "X"
 			when "TM"
-			#$line1 = " |X| "
 			x1[1] = "X"
 			when "TR"
-			#$line1 = " | |X"
 			x1[2] = "X"
 			when "ML"
-			#$line2 = "X| | "
 			x2[0] = "X"
 			when "M"
-			#$line2 = " |X| "
 			x2[1] = "X"
 			when "MR"
-			#$line2 = " | |X"
 			x2[2] = "X"
-			when "BR"
-			#$line3 = "X| | "
+			when "BL"
 			x3[0] = "X"
 			when "BM"
-			#$line3 = " |X| "
 			x3[1] = "X"
-			when "BL"
-			#$line3 = " | |X"
+			when "BR"
 			x3[2] = "X"
+		end		
+		
+		if x1[0] == "X" and x1[1] == "X" and x1[2] == "X"
+			canRun = false
+		elsif x2[0] == "X" and x2[1] == "X" and x2[2] == "X" 
+			canRun = false
+		elsif x3[0] == "X" and x3[1] == "X" and x3[2] == "X" 
+			canRun = false
+		elsif x1[0] == "X" and x2[0] == "X" and x3[0] == "X"
+			canRun = false
+		elsif x1[1] == "X" and x2[1] == "X" and x3[1] == "X"
+			canRun = false
+		elsif x1[2] == "X" and x2[2] == "X" and x3[2] == "X"
+			canRun = false
 		end
 		
-		
 	end
+	
+		line1 = x1[0]+"|"+x1[1]+"|"+x1[2]
+		line2 = x2[0]+"|"+x2[1]+"|"+x2[2]
+		line3 = x3[0]+"|"+x3[1]+"|"+x3[2]
+		puts "\n" + line1
+		puts "------"
+		puts line2
+		puts "------"
+		puts line3 + "\n"
+	
+	puts "Congrats, you win!"
 
 end
 
