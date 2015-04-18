6aa79a80ad3957c037a6e540bfd13006a0aaf518
John <John.Randis1@Marist.edu>
2015-04-18 19:17:22 -0400

    player can now hit

---
+++ Deck.rb
@@ -0,0 +1,32 @@
+require_relative "Card"
+
+class Deck
+  $deck = []
+
+  def initialize()
+  end
+
+  def createCards()
+    for i in 1..13
+      n = String(i)
+      $deck.push(Card.new(n, "\u2665".encode('utf-8')  ) )
+    end
+    for i in 1..13
+      n = String(i)
+      $deck.push(Card.new(n, "\u2660".encode('utf-8')  ) )
+    end
+    for i in 1..13
+      n = String(i)
+      $deck.push(Card.new(n, "\u2666".encode('utf-8')  ) )
+    end
+    for i in 1..13
+      n = String(i)
+      $deck.push(Card.new(n, "\u2663".encode('utf-8')  ) )
+    end
+    return $deck
+  end
+
+  def getCard(num)
+    return @cards[num]
+  end
+end
